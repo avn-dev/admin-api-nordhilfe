84b2487ec7d5ed27eba4926c240a5c323660d6b6
avn-dev <vahap.caliskan@proton.me>
2025-08-24 22:55:37 +0200

    new migration

---
+++ database/migrations/2025_08_24_205510_add_options_to_participantrs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->boolean('vision_test')->default(false)->after('training_session_id');
+            $table->boolean('passport_photos')->default(false)->after('vision_test');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->dropColumn(['vision_test', 'passport_photos']);
+        });
+    }
+};
