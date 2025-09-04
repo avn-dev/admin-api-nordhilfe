4cad1d0f848d487238404d16b954934d514b439a
Abdul Vahap Caliskan <avn@Mac.fritz.box>
2025-09-04 22:51:39 +0200

    added cors handling

---
+++ config/cors.php
@@ -0,0 +1,16 @@
+<?php
+// config/cors.php
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://nordhilfe-hamburg.de',
+        'http://localhost:3000', // dev
+        // add more if needed
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 600,
+    'supports_credentials' => false, // set true only if you need cookies
+];
